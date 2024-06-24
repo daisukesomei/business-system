9e223ef7d8e74da07c98bd2b50f489a4e5a9629d
daisuke.somei <d.somei@leafnet.jp>
2024-06-24 07:49:38 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,15 @@
+@if (Auth::check())
+    {{--ユーザー一覧ページへのリンク--}}
+    <li><a class="link link-hover" href="#">Users</a></li>
+    {{--案件一覧ページへのリンク--}}
+    <li><a class="link link-hover" href="#">案件一覧</a></li>
+    {{--案件登録へのリンク--}}
+    <li><a class="link link-hover" href="#">案件登録</a></li>
+    {{--ログアウトへのリンク--}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
+@else
+    {{--会員登録ページへのリンク--}}
+    <li><a class="link link-hover" href="{{ route('register')}}">会員登録</a></li>
+    {{--ログインページへのリンク--}}
+    <li><a class="link link-hover" href="{{ route('login') }}">ログイン</a></li>
+@endif
